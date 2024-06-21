351e126e9c622e43b28272cec0ef4f6adc2298ff
rauljariasz <rauljariasz@gmail.com>
2024-06-20 23:14:25 -0400

    prueba

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -31,6 +31,7 @@ const Register: React.FC = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    console.log(import.meta.env.VITE_API_URL);
     setError('');
     setSuccess('');
 
