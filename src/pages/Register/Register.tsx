2cd8d230315c3700deeb95c46c81bfb9c98c4a60
rauljariasz <rauljariasz@gmail.com>
2024-06-21 00:05:24 -0400

    Y ahora?

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -51,7 +51,7 @@ const Register: React.FC = () => {
     <main>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Emailllllll:</label>
+          <label>Email:</label>
           <input
             type='email'
             name='email'
